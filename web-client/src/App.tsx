9575bc94979e17a5979a11cdf05eab238fcbeb9c
valours <leflem.valentin@gmail.com>
2019-03-26 00:15:40 +0100

    update router

--- web-client/src/App.tsx
+++ web-client/src/App.tsx
@@ -4,19 +4,21 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import MenuBar from './components/navigation/MenuBar';
 import Home from './pages/Home';
 import Account from './pages/Account';
+import Rentals from './pages/Rentals';
+import House from './pages/House';
 
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
+const App = () => {
+  return (
       <Router>
         <MenuBar/>
-        <Route path="/" component={Home}/>
+        <Route exact path="/" component={Home}/>
+        <Route exact path="/houses/:id" component={House} />
         <Route path="me" component={Account} />
+        <Route path="me/rentals" component={Rentals} />
       </Router>
-    );
-  }
+  )
 }
 
 export default App;
